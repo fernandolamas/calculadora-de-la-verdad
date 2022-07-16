de18e63686bb181ab6a31701069e6b228b33ffac
Zotikram <109394799+Zotikram@users.noreply.github.com>
2022-07-15 21:37:38 -0300

    primera clase

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("Hola mundo, enseñando a zoti a usar github 😁")
+
+num = int(input("decime un numero wachin! "))
+num2 = int(input("decime otro numero! "))
+resultado = num + num2
+print (f"el resultado es {resultado + num2}" )
+
+
